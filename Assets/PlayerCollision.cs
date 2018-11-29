8b56eeea353b6396c738157fa8b97179accdbc22
Kevin <brackkevin@gmail.com>
2018-11-29 17:05:14 -0600

    Logs when the player contacts an object with `Obstacle` tag

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // At first we checked for a name match of
+        // the object that the player collided with.
+        // This works but is taxing on the system.
+
+        // if (collisionInfo.collider.name == "Obstacle")
+        // {
+        //     Debug.Log("We hit an Obstacle");
+
+        // }
+
+        // A better way to do this is to create a
+        // tag and add it to the Obstacle object in unity
+        // This way any object we want can become an 
+        // obstacle and inherit this functionality
+        // simply by adding the object tag
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle");
+        }
+    }
+
+}
