9f5f4f536fc5d66176806bc88247e63ab7ab6371
Kevin <brackkevin@gmail.com>
2018-11-29 16:33:55 -0600

    Adds offset to camera position

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour {
+public class FollowPlayer : MonoBehaviour
+{
 
     public Transform player;
+    public Vector3 offset;
 
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(player.position);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        // Whenever we use lowercase t it
+        // is refers to the game object the script is 
+        // a component on
+        transform.position = player.position + offset;
+    }
 }
