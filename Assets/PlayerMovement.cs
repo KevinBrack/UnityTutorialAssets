19c2c4ef0f1c290f627e2b48cbbf0818cf8880eb
Kevin <brackkevin@gmail.com>
2018-11-29 15:02:15 -0600

    init

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+	
+	// We marked this as "Fixed"Update because we
+    // are updating the physics engine
+	void FixedUpdate () {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+	}
+}
