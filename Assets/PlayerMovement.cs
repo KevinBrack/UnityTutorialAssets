3d57cb636fd3dc7bd248860e7e06597d4b074c14
Kevin <brackkevin@gmail.com>
2018-11-29 15:21:16 -0600

    Adds left and right player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,10 +3,24 @@
 public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 	
 	// We marked this as "Fixed"Update because we
     // are updating the physics engine
 	void FixedUpdate () {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
-	}
+        // add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            // Only executed if contition is met
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
 }
